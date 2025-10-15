6169315a9ef20621fc74f6a0a5d9d41f36b8e2e4
chocopie <chocopie3026@naver.com>
2025-10-15 16:09:32 +0900

    리터럴

--- literal.js
+++ literal.js
@@ -30,6 +30,13 @@ const person = {
 // let firstName = person.firstName;
 let { firstName: fn, lastName: ln, age } = person;
 console.log(fn, ln, age);
+
 // Array Destructuring
 let [ary1, ary2, ...ary3] = getStudentInfo();
 console.log(ary1, ary2, ary3);
+
+// default function parameter
+function minus(n1 = 0, n2 = 0) {
+  return n1 - n2;
+}
+console.log(minus(10));
